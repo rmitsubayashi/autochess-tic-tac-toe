ad2d12c33b053de8dca17a81dfaa30e2a68463fc
rmitsubayashi <ryomitsu@gmail.com>
2020-08-11 19:32:01 +0900

    attacked piece cannot attack back if not in range

--- core/src/com/github/rmitsubayashi/action/piece/Attack.kt
+++ core/src/com/github/rmitsubayashi/action/piece/Attack.kt
@@ -2,6 +2,7 @@ package com.github.rmitsubayashi.action.piece
 
 import com.github.rmitsubayashi.action.*
 import com.github.rmitsubayashi.entity.Piece
+import com.github.rmitsubayashi.game.AttackRangeCalculator
 import com.github.rmitsubayashi.game.Game
 
 class Attack: Action(EmptyEventActor()) {
@@ -16,13 +17,18 @@ class Attack: Action(EmptyEventActor()) {
     override fun execute(game: Game, event: Event, userInput: Piece?): List<Event> {
         val piece = event.actor as Piece
         val enemyPiece = event.actedUpon as Piece
+        val newEvents = mutableListOf<Event>()
         enemyPiece.currHP = enemyPiece.currHP - piece.currStats.attack
-        val enemyDamagedEvent = Event(EventType.pieceDamaged, enemyPiece, null,
+        newEvents.add(Event(EventType.pieceDamaged, enemyPiece, null,
             mapOf(Pair(EventDataKey.AMOUNT, enemyPiece.currHP)))
-        piece.currHP = piece.currHP - enemyPiece.currStats.attack
-        val pieceDamagedEvent = Event(EventType.pieceDamaged, piece, null,
-            mapOf(Pair(EventDataKey.AMOUNT, piece.currHP)))
-        return listOf(enemyDamagedEvent, pieceDamagedEvent)
+        )
+        if (AttackRangeCalculator.isInRange(game.board, piece, enemyPiece)) {
+            piece.currHP = piece.currHP - enemyPiece.currStats.attack
+            newEvents.add(Event(EventType.pieceDamaged, piece, null,
+                    mapOf(Pair(EventDataKey.AMOUNT, piece.currHP)))
+            )
+        }
+        return newEvents
     }
 
     override fun copy(): Attack {
