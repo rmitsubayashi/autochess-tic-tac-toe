1b70837d5c5ffa77736772b21035c07b112b355c
rmitsubayashi <ryomitsu@gmail.com>
2020-08-11 19:58:00 +0900

    made choose race screen cleaner

--- core/src/com/github/rmitsubayashi/ui/chooserace/ChooseRaceScreen.kt
+++ core/src/com/github/rmitsubayashi/ui/chooserace/ChooseRaceScreen.kt
@@ -1,5 +1,6 @@
 package com.github.rmitsubayashi.ui.chooserace
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.github.rmitsubayashi.GdxGame
@@ -12,6 +13,7 @@ import com.github.rmitsubayashi.ui.game.GameScreen
 import com.github.rmitsubayashi.ui.util.IStageScreen
 import com.github.rmitsubayashi.ui.util.UIClickListener
 import com.github.rmitsubayashi.ui.util.appSkin
+import com.github.rmitsubayashi.ui.util.setBackgroundColor
 
 class ChooseRaceScreen(private val game: GdxGame): IStageScreen(game) {
     private val options = UIRaceOptions()
@@ -33,7 +35,10 @@ class ChooseRaceScreen(private val game: GdxGame): IStageScreen(game) {
         )
         val table = Table()
         table.setFillParent(true)
-        table.add(options).row()
+        table.setBackgroundColor(appSkin.getColor("white"))
+        val instructionsLabel = Label("Choose a starting race", appSkin.get("big", Label.LabelStyle::class.java))
+        table.add(instructionsLabel).padBottom(24f).row()
+        table.add(options).padBottom(10f).row()
         table.add(submitButton)
         this.stage.addActor(table)
 
--- core/src/com/github/rmitsubayashi/ui/chooserace/UIRaceOptions.kt
+++ core/src/com/github/rmitsubayashi/ui/chooserace/UIRaceOptions.kt
@@ -1,27 +1,33 @@
 package com.github.rmitsubayashi.ui.chooserace
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.github.rmitsubayashi.entity.Race
 import com.github.rmitsubayashi.ui.util.UIClickListener
 import com.github.rmitsubayashi.ui.util.appSkin
 
 class UIRaceOptions: Table() {
     private var selection: Race? = null
+    private val allOptions = mutableListOf<TextButton>()
     fun getSelection(): Race? {
         return selection
     }
 
     fun setOptions(races: List<Race>) {
         for (race in races) {
-            val label = Label(race.name, appSkin)
-            label.addListener(
-                    UIClickListener(label,
+            val textButton = TextButton(race.name, appSkin.get("borderless", TextButton.TextButtonStyle::class.java))
+            textButton.addListener(
+                    UIClickListener(textButton,
                             {
                                 selection = race
+                                for (option in allOptions) {
+                                    option.isChecked = false
+                                }
+                                textButton.isChecked = true
                             })
             )
-            this.add(label)
+            this.add(textButton).row()
+            allOptions.add(textButton)
         }
     }
 }
